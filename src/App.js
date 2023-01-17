0402dbec81d2de510b5fb2148a1d878c5d531ba3
naveenraj1712 <neevankeyboard@gmail.com>
2023-01-17 22:43:25 +0530

    fixed style issue ui

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
 // import logo from './logo.svg';
-import './App.css';
-import { BrowserRouter as Router } from 'react-router-dom';
-import {StylesProvider} from '@material-ui/core';
+import "./App.css";
+import { BrowserRouter as Router } from "react-router-dom";
+import { StylesProvider } from "@material-ui/core";
 // import {MuiPickersUtilsProvider} from '@material-ui/pickers';
 // import DateFnsUtils from '@date-io/date-fns'
-import LoginPage from './components/LoginPage';
-import Header from './components/Header';
+import LoginPage from "./components/LoginPage";
+import Header from "./components/Header";
 
 function App() {
   return (
     <Router>
       <StylesProvider injectFirst>
-        <Header/>
-        {/* <MuiPickersUtilsProvider utils={DateFnsUtils}> */}
-          <LoginPage/>
-        {/* </MuiPickersUtilsProvider> */}
+        <div className="root-img">
+          <Header />
+          <LoginPage />
+        </div>
       </StylesProvider>
     </Router>
   );
