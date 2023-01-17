d846c6a856a658a93667daa9b9426ab2e04e5f77
naveenraj1712 <neevankeyboard@gmail.com>
2023-01-17 21:06:10 +0530

    Login page UI modified

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,24 @@
 import { Box, Button, Typography } from "@material-ui/core";
-import UPLogo from "../assets/logo.svg";
+import UPLogo from "../assets/logoorangeblack3.svg";
+import "./Header.scss";
 
 const Header = () => {
-    return(
-        <Box style={{display: "flex", width: "100%", height: "50px", background: "linear-gradient(to right bottom, #05C033, #02C6C3)"}}>
-            <div style={{width: "65px"}}>
-            <img src={UPLogo} alt="Upcoming Entertainers" style={{height: "125px", width: "125px", position: "relative", top: "-16px", left: "-28px"}}/>
-            </div>
-            <Typography component="h1" style={{fontSize: "24px", padding: "5px", marginTop: "2px"}}>Upcoming Entertainers</Typography>
-            <Button variant="text" style={{display: "flex", marginLeft: "auto"}}>Login</Button>
-            <Button variant="text" style={{marginRight: "10px"}}>Signup</Button>
-        </Box>
-    )
-}
+  return (
+    <Box className="header-root">
+      <div className="logo-root">
+        <img src={UPLogo} alt="Upcoming Entertainers" className="logo-image" />
+      </div>
+      <Typography component="h1" className="header-title">
+        Upcoming Entertainers
+      </Typography>
+      <Button variant="text" className="header-button-1">
+        Login
+      </Button>
+      <Button variant="text" className="header-button-2">
+        Signup
+      </Button>
+    </Box>
+  );
+};
 
 export default Header;
--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -1,19 +1,54 @@
-import { Button, FormControl, TextField } from "@material-ui/core";
+import { Box, Button, FormControl, TextField, Typography } from "@material-ui/core";
 import { useState } from "react";
+import "./LoginPage.scss";
 
 const LoginPage = () => {
-    const [userCredentials, setUserCredentials] = useState({userName: '', password: ''});
-    const onChangeInput = (e) => {
-        console.log(e.target);
-        setUserCredentials({...userCredentials, [e.target.name]: e.target.value});
-    }
-    return (
-        <FormControl variant="standard">
-            <TextField value = {userCredentials.userName} label="User Name" name="userName" onChange={onChangeInput}/>
-            <TextField value = {userCredentials.password} label="Password" name="password" onChange={onChangeInput}/>
-            <Button variant="submit" onClick={()=>{console.log("Logged In")}}>Login</Button>
+  const [userCredentials, setUserCredentials] = useState({
+    userName: "",
+    password: "",
+  });
+  const onChangeInput = (e) => {
+    console.log(e.target);
+    setUserCredentials({ ...userCredentials, [e.target.name]: e.target.value });
+  };
+  return (
+    <Box className="login-page">
+      <Box className="login-page-root-img" />
+      <Box className="login-page-root">
+        <FormControl variant="standard" className="login-form-root">
+        <Typography className="login-page-header" component="h2">Login Page</Typography>
+          <TextField
+            InputLabelProps={{className: `login-form-user-label ${userCredentials.userName ? "" : "login-form-input-pad"}`}}
+            inputProps={{className: "login-form-user-input"}}
+            value={userCredentials.userName}
+            label="User Name"
+            name="userName"
+            onChange={onChangeInput}
+            className="login-form-user"
+          />
+          <TextField
+            InputLabelProps={{className: `login-form-user-label ${userCredentials.password ? "" : "login-form-input-pad"}`}}
+            inputProps={{className: "login-form-user-input"}}
+            value={userCredentials.password}
+            label="Password"
+            name="password"
+            onChange={onChangeInput}
+            className="login-form-user"
+            type="password"
+          />
+          <Button
+            variant="submit"
+            onClick={() => {
+              console.log("Logged In");
+            }}
+            className="login-form-submit"
+          >
+            Login
+          </Button>
         </FormControl>
-    )
-}
+      </Box>
+    </Box>
+  );
+};
 
 export default LoginPage;
